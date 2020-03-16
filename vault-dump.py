1842c4dcb40a6371f6c9f42981ee204432774043
Marvin Keller <marvin.keller@europace.de>
2020-03-16 11:35:51 -0300

    removed unneeded imports, read token from env, removed unneded outputs

--- vault-dump.py
+++ vault-dump.py
@@ -15,26 +15,21 @@
 #
 # Copyright (c) 2017 Shane Ramey <shane.ramey@gmail.com>
 # Licensed under the Apache License, Version 2.0
-from __future__ import print_function
+
 import sys
-import subprocess
 import os
-import pwd
 import hvac
 import datetime
 
 def print_header():
-    user = pwd.getpwuid(os.getuid()).pw_name
     date = "{} UTC".format(datetime.datetime.utcnow())
     vault_address = os.environ.get('VAULT_ADDR')
-    top_vault_prefix = os.environ.get('TOP_VAULT_PREFIX','/secret/')
 
     print ('#')
     print ('# vault-dump.py backup')
-    print ("# dump made by {}".format(user))
     print ("# backup date: {}".format(date))
     print ("# VAULT_ADDR env variable: {}".format(vault_address))
-    print ("# TOP_VAULT_PREFIX env variable: {}".format(top_vault_prefix))
+    # print ("# TOP_VAULT_PREFIX env variable: {}".format(top_vault_prefix))
     print ('# STDIN encoding: {}'.format(sys.stdin.encoding))
     print ('# STDOUT encoding: {}'.format(sys.stdout.encoding))
     print ('#')
@@ -43,35 +38,31 @@ def print_header():
 
 # looks at an argument for a value and prints the key
 #  if a value exists
-def recurse_for_values(path_prefix, candidate_key):
-    candidate_values = candidate_key['data']['keys']
-    for candidate_value in candidate_values:
-        next_index = path_prefix + candidate_value
-        if candidate_value.endswith('/'):
-            next_value = client.list(next_index)
-            recurse_for_values(next_index, next_value)
-        else:
-            stripped_prefix=path_prefix[:-1]
-            final_dict = client.read(next_index)['data']
-            print ("\nvault write {}".format(next_index), end='')
+# def recurse_for_values(path_prefix, candidate_key):
+#     candidate_values = candidate_key['data']['keys']
+#     for candidate_value in candidate_values:
+#         next_index = path_prefix + candidate_value
+#         if candidate_value.endswith('/'):
+#             next_value = client.list(next_index)
+#             recurse_for_values(next_index, next_value)
+#         else:
+#             stripped_prefix=path_prefix[:-1]
+#             final_dict = client.read(next_index)['data']
+#             print ("\nvault write {}".format(next_index), end='')
 
-            sorted_final_keys = sorted(final_dict.keys())
-            for final_key in sorted_final_keys:
-                final_value = final_dict[final_key]
-                try:
-                    final_value = final_value.encode("utf-8")
-                except AttributeError:
-                    final_value = final_value
-                print (" {0}={1}".format(final_key, repr(final_value)), end='')
+#             sorted_final_keys = sorted(final_dict.keys())
+#             for final_key in sorted_final_keys:
+#                 final_value = final_dict[final_key]
+#                 try:
+#                     final_value = final_value.encode("utf-8")
+#                 except AttributeError:
+#                     final_value = final_value
+#                 print (" {0}={1}".format(final_key, repr(final_value)), end='')
 
-
-env_vars = os.environ.copy()
-hvac_token = subprocess.check_output(
-    "vault read -field id auth/token/lookup-self",
-    shell=True,
-    env=env_vars)
 
 hvac_url = os.environ.get('VAULT_ADDR','http://localhost:8200')
+hvac_token = os.environ.get('VAULT_TOKEN','http://localhost:8200')
+
 hvac_client = {
     'url': hvac_url,
     'token': hvac_token,
@@ -79,8 +70,6 @@ hvac_client = {
 client = hvac.Client(**hvac_client)
 assert client.is_authenticated()
 
-top_vault_prefix = os.environ.get('TOP_VAULT_PREFIX','/secret/')
-
 print_header()
-top_level_keys = client.list(top_vault_prefix)
-recurse_for_values(top_vault_prefix, top_level_keys)
+# top_level_keys = client.list(top_vault_prefix)
+# recurse_for_values(top_vault_prefix, top_level_keys)
